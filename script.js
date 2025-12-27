dc94beea7218c8f425bbe1a11abae5eebf4e2f24
Flossy-elephant <samyakahire2006@gmail.com>
2025-12-27 19:01:56 +0530

    first prototype using only vanilla js ,small dictionary of words

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let gameState = {
 }
 
 const typingInput = document.getElementById('typingInput')
-const wordsContainer = document.getElementById('wordsContainer')
+const wordsContainer = document.getElementById('wordContainer')
 const resetBtn = document.getElementById('resetBtn')
 const wpmDisplay = document.getElementById('wpm')
 const accuracyDisplay = document.getElementById('accuracy')
@@ -54,5 +54,84 @@ function handleInput(event){
 
   const input = event.target.value
 
-  if(!gameState.isActive )
+  if(!gameState.isActive && input.length > 0){
+    gameState.isActive = true
+    gameState.startTime = Date.now()
+  }
+
+  if(input.endsWith(' ')){
+    const typedWord = input.trim()
+    gameState.typedWords.push(typedWord)
+    gameState.currentWordIndex++
+    typingInput.value = ' '
+    
+    if(gameState.currentWordIndex >= words.length){
+      endTest()
+      return
+    }
+
+    displayWords()
+    
+  }
 }
+
+function updateStats(){
+if(!gameState.isActive || gameState.startTime === null){
+  return
+}
+
+const elapsedSeconds = (Date.now() - gameState.startTime)/1000
+const elapsedMinutes = elapsedSeconds / 60
+
+const wpm = Math.round((gameState.currentWordIndex/elapsedMinutes) || 0)
+
+let correctWords = 0
+for(let i = 0 ; i < gameState.typedWords.length ; i++){
+  if(gameState.typedWords[i] === words[i]){
+    correctWords++
+  }
+}
+
+const accuracy = gameState.typedWords.length > 0
+? Math.round((correctWords / gameState.typedWords.length) * 100)
+: 100
+
+wpmDisplay.textContent = wpm
+accuracyDisplay.textContent = accuracy + '%'
+timeDisplay.textContent = Math.floor(elapsedSeconds) + 's'
+}
+
+function endTest(){
+  updateStats()
+  gameState.isActive = false
+  typingInput.disabled = true
+  typingInput.placeholder = 'Test finished! Click reset to try again. '
+  
+}
+
+function resetTest(){
+  gameState = {
+    currentWordIndex: 0,
+    typedWords: [],
+    startTime: null,
+    isActive: false,
+    correctChars: 0,
+    totalChars: 0
+  }
+
+  typingInput.value = ''
+  typingInput.disabled = false
+  typingInput.placeholder = 'Click here and start typing....'
+  typingInput.focus()
+
+  wpmDisplay.textContent = '0'
+  accuracyDisplay.textContent = '100%'
+  timeDisplay.textContent = '0s'
+
+  displayWords()
+}
+
+typingInput.addEventListener('input',handleInput)
+resetBtn.addEventListener('click',resetTest)
+
+init()
